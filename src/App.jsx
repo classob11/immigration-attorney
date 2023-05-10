e9b743f306c6c02c58c5938b21895dddcc553e1b
classob11 <99842930+classob11@users.noreply.github.com>
2023-05-10 10:43:47 -0600

    enable

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Header from "./components/header/header";
 import About from "./components/about/about";
-import Certificate from "./components/certificates/certificates";
 import "./App.css";
 
 const App = () => {
